d88854310458b4e533692c6a45db2474c2d3d2a0
repl.it user <replituser@example.com>
2020-06-30 11:25:51 +0000

    Added the menu

--- bills.py
+++ bills.py
@@ -1,52 +1,6 @@
 import os
 # here all classes that represent bills are defined
 
-ans=True
-while ans:
-    print ("""
-    1.Anual Bills
-    2.Monthly Bills
-    3.Other Bills
-    4.Exit/Quit
-    """)
-    ans=input(">>What option would you like to choose? ") 
-    if ans=="1": 
-      print("\n -Anual Bills- opened") 
-    elif ans=="2":
-      print("\n -Monthly Bills- opened") 
-    elif ans=="3":
-      print("-Other Bills-")
-      print("""
-      1.Transportation 
-      2.Other
-      """) 
-      ans2=input(">>Which category would you like to access? ")
-      if ans2=="1":
-        print("""
-        1.PlaneTickets
-        2.FerryTickets
-        3.TrainTickets
-        4.VanTickets
-        """)
-        print("CATEGORY-TRANSPORTATION")
-        ans3 = input("Which information would you like to access? ")
-      if ans2=="2":
-        print("""
-        1.ChemicalCleaning
-        2.OnlineLibrary
-        3.CarRent
-        4.ConcertTickets
-        5.Handyman
-        """)
-        print("CATEGORY-OTHER")
-        ans4 = input("Which information would you like to access? ")
-    elif ans=="4":
-      print("\n Goodbye")
-      ans = None 
-      break
-    elif ans !="":
-      print("\n Not Valid Choice Try again")  
-
 
 class BillBase:
     def __init__(self,billID,billType,year,month,day,payvalue):
